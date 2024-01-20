42eddd70ac550c0c007ab0be641e124686e27b45
Jatin4224 <jaikumarsharma94130@gmail.com>
2024-01-20 15:18:23 +0530

    created Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,16 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+import {
+  About,
+  Cart,
+  Checkout,
+  Landing,
+  Login,
+  Products,
+  Error,
+  HomeLayout,
+  Register,
+  SingleProduct,
+} from "./pages";
+const App = () => {
+  return <h1 className="text-3xl font-bold underline"> Hellow world</h1>;
+};
+export default App;
